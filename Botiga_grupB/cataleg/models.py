b7cfa68cae43a2dbc6ef8984abd574bfcd61eb47
Oscar Pérez <2023_oscar.perez@iticbcn.cat>
2024-04-12 16:21:04 +0200

    Creat aplicació cataleg amb el model de producte y cataleg

---
+++ Botiga_grupB/cataleg/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Producte(models.Model):
+    nom = models.CharField(max_length=30)
+    marca = models.CharField(max_length=30)
+    preu = models.FloatField
+    estoc = models.IntegerField
+    gama = models.CharField(max_length=30)
+    pes = models.FloatField
+
+class Cataleg(models.Model):
+    id_producte = models.ForeignKey(Producte, on_delete=models.CASCADE)
