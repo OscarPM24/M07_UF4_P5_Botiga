9510562a3b2542588a40e604aa9c1434ad1c41e7
unknown <adriagp90@gmail.com>
2024-04-19 16:37:14 +0200

    afegit UsuariConfig a settings de Botiga

--- Botiga_grupB/Botiga_grupB/settings.py
+++ Botiga_grupB/Botiga_grupB/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cataleg.apps.CatalegConfig',
     'carreto.apps.CarretoConfig',
+    'usuari.apps.UsuariConfig',
     'rest_framework',
 ]
 
