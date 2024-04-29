95cf9d340aef48b790cda84618eaaf11e1afd96e
Oscar Pérez <2023_oscar.perez@iticbcn.cat>
2024-04-29 18:55:42 +0200

    Actualitzar productes del cataleg en /cataleg/<id_cataleg>/editar/<id_prod>

--- Botiga_grupB/cataleg/urls.py
+++ Botiga_grupB/cataleg/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.catalegs, name='catalegs'),
     path('<int:id>', views.cataleg, name='cataleg'),
     path('<int:id>/afegir/', views.afegirProducte, name='afegir'),
-    path('<int:id_cataleg>/eliminar/<int:id_prod>', views.eliminarProducte, name='eliminar')
+    path('<int:id_cataleg>/eliminar/<int:id_prod>', views.eliminarProducte, name='eliminar'),
+    path('<int:id_cataleg>/editar/<int:id_prod>', views.editarProducte, name='editar')
 ]
--- Botiga_grupB/cataleg/views.py
+++ Botiga_grupB/cataleg/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework.renderers import BrowsableAPIRenderer, JSONRenderer
 from .models import Producte, Cataleg
@@ -41,6 +42,22 @@ def eliminarProducte(request, id_cataleg, id_prod):
     queryset = Cataleg.objects.get(id=id_cataleg)
     if request.method == "DELETE":
         queryset.productes.remove(id_prod)
+
+    queryset = Cataleg.objects.get(id=id_cataleg)
+    serializer = CatalegSerializer(queryset)
+    return Response({"Cataleg": serializer.data})
+
+@api_view(["GET", "PUT"])
+@renderer_classes([BrowsableAPIRenderer, JSONRenderer])
+def editarProducte(request, id_cataleg, id_prod):
+    print("editando")
+    queryset = Cataleg.objects.get(id=id_cataleg)
+    if request.method == "PUT":
+        dades = request.data
+        producte = Producte.objects.get(id=id_prod)
+        for key, value in dades.items():
+            setattr(producte, key, value)
+        producte.save()
 
     queryset = Cataleg.objects.get(id=id_cataleg)
     serializer = CatalegSerializer(queryset)
