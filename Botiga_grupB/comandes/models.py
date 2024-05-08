4d551c898979456398ae55fcf78088e00c9892b8
unknown <adriagp90@gmail.com>
2024-05-08 17:05:28 +0200

    afegit comentaris orientatius

--- Botiga_grupB/comandes/models.py
+++ Botiga_grupB/comandes/models.py
@@ -3,5 +3,6 @@ from django.db import models
 # Create your models here.
 class Comandes(models.Model):
     """
-
+        comandes/historic -> mostra l'historial de compres realitzades
+        comandes/pendents -> mostra els carretons sense pagar
     """
